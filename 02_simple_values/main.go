39347c3b21d050015fbf973220473d809f8f6120
sumit <suberquercusumit@gmail.com>
2024-07-12 18:27:25 +0530

    add simple values

---
+++ 02_simple_values/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 👉 int
+	fmt.Println(1 + 1)
+	// 👉 String
+	fmt.Println("Hello golang")
+	// 👉 boolean
+	fmt.Println(true)
+	fmt.Println(false)
+	// 👉 floats
+	fmt.Println(10.5)
+	fmt.Println(10.34)
+	fmt.Println(7.0 / 3.0)
+
+}
