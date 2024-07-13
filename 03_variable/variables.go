dae62ef5314a6a903260d5ef970201b6fe5797dd
sumit <suberquercusumit@gmail.com>
2024-07-13 18:22:26 +0530

    add variables

---
+++ 03_variable/variables.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var name = "golang" //infer type
+	// var name string = "golang"
+	// fmt.Println(name)
+
+	// var isAdult = true
+	// var isAdult = false
+	// fmt.Println(isAdult)
+
+	// var age int = 30
+	// fmt.Println(age)
+
+	//shorthand syntax
+	// name := "golang"
+
+	// fmt.Println(name)
+
+	// var name string // //define variable like
+
+	// name = "golang"
+
+	// fmt.Println(name)
+
+	// var price float32 = 50.5
+	// fmt.Println(price)
+
+	// var price = 50.5
+	// fmt.Println(price)
+
+	price := 50.8
+	fmt.Println(price)
+
+}
