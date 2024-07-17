8431a0e6cdebdae1e2b8e29967d048b5d722cd79
sumit <suberquercusumit@gmail.com>
2024-07-17 15:34:21 +0530

    add switch case golang

---
+++ 07_Switch_Statement/switch.go
@@ -0,0 +1,50 @@
+package main
+
+func main() {
+
+	// simple switch
+
+	// i := 5
+	// i := 3
+	// switch i {
+	// case 1:
+	// 	fmt.Println("one")
+	// case 2:
+	// 	fmt.Println("two")
+	// case 3:
+	// 	fmt.Println("three")
+	// default:
+	// 	fmt.Println("other")
+	// }
+
+	//multiple condition switch
+
+	// switch time.Now().Weekday() {
+	// case time.Saturday, time.Sunday:
+	// 	fmt.Println("It's weeend")
+	// default:
+	// 	fmt.Println("It's workday")
+	// }
+
+	// type switch
+	// whoAmI := func(i interface{}) { // interface use to any type for
+	// 	// switch t := i.(type) {
+	// 	switch i.(type) {
+	// 	case int:
+	// 		fmt.Println("it's an integer")
+	// 	case string:
+	// 		fmt.Println("it's a string")
+	// 	case bool:
+	// 		fmt.Println("it's a boolean")
+	// 	default:
+	// 		// fmt.Println("other", t)
+	// 		fmt.Println("other")
+	// 	}
+	// }
+
+	// whoAmI("golang")
+	// whoAmI(50)
+	// whoAmI(true)
+	// whoAmI(55.20)
+
+}
